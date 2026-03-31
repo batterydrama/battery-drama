d42a6af25d0507f0071ee9416a8fde231a4b6424
Sandhya Jaswal <HELLO@BATTERYDRAMA.COM>
2026-03-31 15:14:32 +1100

    Add Mac build workflow

--- forge.config.js
+++ forge.config.js
@@ -2,9 +2,7 @@ const { FusesPlugin } = require('@electron-forge/plugin-fuses');
 const { FuseV1Options, FuseVersion } = require('@electron/fuses');
 
 module.exports = {
-  packagerConfig: {
-    asar: true,
-  },
+  packagerConfig: {},
   rebuildConfig: {},
   makers: [
     {
@@ -26,21 +24,20 @@ module.exports = {
   ],
   plugins: [
     {
+      name: '@electron-forge/plugin-auto-unpack-natives',
+      config: {},
+    },
+    {
       name: '@electron-forge/plugin-vite',
       config: {
-        // `build` can specify multiple entry builds, which can be Main process, Preload scripts, Worker process, etc.
-        // If you are familiar with Vite configuration, it will look really familiar.
         build: [
           {
-            // `entry` is just an alias for `build.lib.entry` in the corresponding file of `config`.
             entry: 'src/main.js',
             config: 'vite.main.config.mjs',
-            target: 'main',
           },
           {
             entry: 'src/preload.js',
             config: 'vite.preload.config.mjs',
-            target: 'preload',
           },
         ],
         renderer: [
@@ -51,8 +48,6 @@ module.exports = {
         ],
       },
     },
-    // Fuses are used to enable/disable various Electron functionality
-    // at package time, before code signing the application
     new FusesPlugin({
       version: FuseVersion.V1,
       [FuseV1Options.RunAsNode]: false,
